572fbbe430b6d2ae7cdb3d1afaef790fbec3d067
King <24613526+nil-omen@users.noreply.github.com>
2026-03-28 16:25:29 +0200

    feat: initialize project structure with basic HTML, CSS, and JavaScript demonstration files

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+document.getElementById("myH1").textContent = "Hello There!";
+console.log(document.getElementById("p1").textContent + " This is a test");
+// alert("This is an alert");
+
+let x = 123
+let y = "Nice"
+console.log(typeof x)
+console.log(typeof y)
+
+const pi = 3.14159
+console.log(`${y} to meet you`)
+
+console.log("-------------")
+
+document.getElementById("p2").textContent = `testing 'typeof' of x ${typeof x}`;
+
+let happy = false
+
+document.getElementById("p3").textContent = `This is a the type of happy ${typeof happy}`;
---
+++ README.md
@@ -0,0 +1,15 @@
+# Learning Web (HTML, CSS, JS)
+
+This repo is for learning web development (HTML, CSS, JS).
+
+## Courses
+
+### JavaScript
+
+[JavaScript Full Course (Video) By Bro Code](https://www.youtube.com/watch?v=lfmg-EJ8gm4)
+
+[Eloquent JavaScript (Book)](https://eloquentjavascript.net/)
+
+### HTML & CSS
+
+[HTML & CSS Full Course (Video) By Bro Code](https://www.youtube.com/watch?v=HGTJBPNC-Gw)
